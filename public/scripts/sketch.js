bceb4575bff7d0150d4b3fd43a4fc08a33483d4e
fsImageries <imageries@mail.de>
2021-10-21 09:54:03 +0200

    Worked on p5 example in optigame1-2.

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -18,14 +18,39 @@ function setup() {
 }
 
 function draw() {
-  background(127);
+  const tri = width / 3;
+  const offset = tri / 2;
+  const dispHeight = 100;
+  const newDist = (tri * 3 - offset - width/2) / 2
+
+  background($(":root").css("--CardBrown"));
+  noFill();
+  stroke(10);
+  strokeWeight(10);
+
+  circle((tri - offset) + newDist, height / 2, dispHeight);
+
+  triangle(
+    tri * 2 - offset - dispHeight / 2,
+    height / 2 + dispHeight / 2,
+    tri * 2 - offset + dispHeight / 2,
+    height / 2 + dispHeight / 2,
+    tri * 2 - offset,
+    height / 2 - dispHeight / 2
+  );
+
+  rectMode(CENTER);
+  rect((tri * 3 - offset) - newDist, height / 2, dispHeight);
+
+  rectMode(CORNER);
+  // background(127);
   fill($(":root").css("--GuardRed"));
   rect(-width / 2 - push, 0, width, height);
 
   fill($(":root").css("--GuardRed"));
   rect(width / 2 + push, 0, width, height);
 
-  push = anim ? push + 1 : push;
+  push = anim ? push + 2 : push;
   // some++;
 }
 
